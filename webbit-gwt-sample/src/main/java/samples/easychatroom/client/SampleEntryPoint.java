8c59124583ef56644d678504060f5157cb2a50a7
Colin Alworth <niloc132@gmail.com>
2011-10-16 22:02:23 -0500

    Initial commit with basic poms, and a sample webbit project that compiles GWT into the contents of the jar, and allows it to run once the server is launched

---
+++ webbit-gwt-sample/src/main/java/samples/easychatroom/client/SampleEntryPoint.java
@@ -0,0 +1,36 @@
+/**
+ *  Copyright 2011 Colin Alworth
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package samples.easychatroom.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * @author colin
+ *
+ */
+public class SampleEntryPoint implements EntryPoint {
+
+	/* (non-Javadoc)
+	 * @see com.google.gwt.core.client.EntryPoint#onModuleLoad()
+	 */
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
